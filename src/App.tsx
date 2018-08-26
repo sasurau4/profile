dad92c7e04f1d4cfaed4b5c040f377e432facf5e
Daiki Ihara <sasurau4@gmail.com>
2018-08-26 22:50:27 +0900

    [WIP] add route sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  RouteComponentProps,
+} from 'react-router-dom';
 import './App.css';
 
 import logo from './logo.svg';
@@ -14,9 +20,76 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <BasicExample />
       </div>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topic = (props: RouteComponentProps<any>) => (
+  <div>
+    <h3>{props.match.params.topicId}</h3>
+  </div>
+);
+
+const Topics = (props: RouteComponentProps<any>) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${props.match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${props.match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${props.match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${props.match.path}/:topicId`} component={Topic} />
+    <Route
+      exact={true}
+      path={props.match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
 
 export default App;
