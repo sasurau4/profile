9114ba776da8d5a93281c9ee9cfca8c836918e0c
Daiki Ihara <sasurau4@gmail.com>
2018-08-29 23:55:27 +0900

    implement Header component with react-router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,39 @@ import AppBar from '@material-ui/core/AppBar';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { RouteComponentProps, withRouter } from 'react-router';
 
-const Header: React.StatelessComponent = () => (
-  <header>
-    <AppBar>
-      <nav>
-        <Tabs value={false}>
-          <Link to="/">
-            <Tab label="Home" />
-          </Link>
-          <Link to="about">
-            <Tab label="Home" />
-          </Link>
-          <Link to="/topics">
-            <Tab label="Topics" />
-          </Link>
-        </Tabs>
-      </nav>
-    </AppBar>
-  </header>
-);
+interface State {
+  path: string;
+}
 
-export default Header;
+class Header extends React.Component<RouteComponentProps<{}>, State> {
+  public state = {
+    path: '/',
+  };
+
+  public handleChange = ({}, value: string) => {
+    const path: string = value;
+    this.props.history.push(path);
+    this.setState({ path });
+  };
+
+  public render() {
+    const { path } = this.state;
+    return (
+      <header>
+        <AppBar>
+          <nav>
+            <Tabs value={path} onChange={this.handleChange}>
+              <Tab label="Home" value="/" />
+              <Tab label="About" value="/about" />
+              <Tab label="Topics" value="/topics" />
+            </Tabs>
+          </nav>
+        </AppBar>
+      </header>
+    );
+  }
+}
+
+export default withRouter(Header);
