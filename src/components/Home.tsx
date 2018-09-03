bac2d52766f30202381f467a0593b4c25c69bdd8
Daiki Ihara <sasurau4@gmail.com>
2018-09-03 13:19:26 +0900

    add background image for home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,10 +6,14 @@ import {
 } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
+import Background from '../images/kirigamine-10_TP_V.jpg';
 
 const styles = (theme: Theme) =>
   createStyles({
     appBarSpacer: theme.mixins.toolbar,
+    backgroundContainer: {
+      backgroundImage: `url(${Background})`,
+    },
   });
 
 interface Props extends WithStyles<typeof styles> {}
@@ -17,9 +21,11 @@ interface Props extends WithStyles<typeof styles> {}
 const Home = withStyles(styles)((props: Props) => (
   <>
     <div className={props.classes.appBarSpacer} />
-    <Typography variant="display1" gutterBottom={true}>
-      This is Home
-    </Typography>
+    <div className={props.classes.backgroundContainer}>
+      <Typography variant="display1" gutterBottom={true}>
+        This is Home
+      </Typography>
+    </div>
   </>
 ));
 
