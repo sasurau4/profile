17b49a2a834c947f803d4c9563e3c394b3b986be
Daiki Ihara <sasurau4@gmail.com>
2018-09-04 23:30:19 +0900

    add logo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,45 @@
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
+import AndroidLogo from '../images/android_logo.gif';
 
-export default function About() {
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
+
+const styles = () =>
+  createStyles({
+    avatar: {
+      height: 200,
+      width: 200,
+    },
+    grid: {
+      minHeight: '100vh',
+    },
+  });
+
+interface Props extends WithStyles<typeof styles> {}
+
+export default withStyles(styles)(function About(props: Props) {
+  const { classes } = props;
   return (
-    <div>
-      <h2>About</h2>
-    </div>
+    <Paper id="about">
+      <Grid
+        container={true}
+        justify="center"
+        direction="column"
+        alignItems="center"
+        className={classes.grid}
+      >
+        <Typography variant="display3" align="center">
+          About
+        </Typography>
+        <Avatar
+          alt="Daiki Ihara"
+          src={AndroidLogo}
+          className={classes.avatar}
+        />
+      </Grid>
+    </Paper>
   );
-}
+});
