8219e29d6ab429440ad455df4d2f66184e4e16b3
Daiki Ihara <sasurau4@gmail.com>
2018-09-06 21:40:58 +0900

    fix About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,27 @@
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles,
+} from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
-import AndroidLogo from '../images/android_logo.gif';
+import Androidfy from '../images/my-androidify.png';
+import MyPicture from '../images/my-profile-picture.jpg';
 
-import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
-
-const styles = () =>
+const styles = (theme: Theme) =>
   createStyles({
     avatar: {
       height: 200,
       width: 200,
     },
     grid: {
+      maxWidth: '100vw',
       minHeight: '100vh',
     },
+    toolbarSpacer: theme.mixins.toolbar,
   });
 
 interface Props extends WithStyles<typeof styles> {}
@@ -23,52 +29,57 @@ interface Props extends WithStyles<typeof styles> {}
 export default withStyles(styles)(function About(props: Props) {
   const { classes } = props;
   return (
-    <Paper id="about">
+    <>
+      <div className={classes.toolbarSpacer} id="about" />
       <Grid
         container={true}
         justify="center"
         direction="column"
         alignItems="center"
         className={classes.grid}
+        spacing={32}
       >
-        <Typography variant="display3" align="center">
-          About
-        </Typography>
-        <Grid
-          container={true}
-          justify="center"
-          direction="row"
-          alignItems="center"
-          spacing={24}
-        >
-          <Grid item={true}>
-            <Avatar
-              alt="Daiki Ihara"
-              src={AndroidLogo}
-              className={classes.avatar}
-            />
-          </Grid>
-          <Grid item={true}>
-            <Avatar
-              alt="Daiki Ihara"
-              src={AndroidLogo}
-              className={classes.avatar}
-            />
+        <Grid item={true}>
+          <Typography variant="display3" align="center">
+            About
+          </Typography>
+        </Grid>
+        <Grid item={true}>
+          <Grid
+            container={true}
+            justify="center"
+            direction="row"
+            alignItems="center"
+            spacing={16}
+          >
+            <Grid item={true}>
+              <Avatar
+                alt="Daiki Ihara androidfy"
+                src={Androidfy}
+                className={classes.avatar}
+              />
+            </Grid>
+            <Grid item={true}>
+              <Avatar
+                alt="Daiki Ihara picture"
+                src={MyPicture}
+                className={classes.avatar}
+              />
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item={true} xs={6}>
+        <Grid item={true} xs={8} sm={6}>
           <Typography
             variant="title"
             align="center"
             color="textSecondary"
             paragraph={true}
           >
-            Something short and leading about the collection below—its contents,
-            the creator, etc. Make it short and sweet, but not too short so
-            folks don&apos;t simply skip over it entirely.
+            Daiki Ihara is Front-End Engineer. I love Web, Vim, Javascript,
+            Typescript, React etc. Working in Tokyo, Japan. Like web services.
           </Typography>
         </Grid>
       </Grid>
-    </Paper>
+    </>
   );
 });
