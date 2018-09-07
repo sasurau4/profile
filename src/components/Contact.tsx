568ed8caa94aa464c8cf1010b0c5d3c337636d09
Daiki Ihara <sasurau4@gmail.com>
2018-09-07 22:59:06 +0900

    fix Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,5 @@
-import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import {
   createStyles,
   Theme,
@@ -7,33 +7,69 @@ import {
   WithStyles,
 } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import classNames from 'classnames';
 import { GithubCircle, Linkedin, Twitter } from 'mdi-material-ui';
 import * as React from 'react';
 
 const styles = (theme: Theme) =>
   createStyles({
-    avatar: {
-      height: 80,
-      width: 80,
-    },
+    githubIcon: { color: '#333' },
     grid: {
       maxWidth: '100vw',
       minHeight: '100vh',
     },
     icon: {
-      fontSize: 40,
+      fontSize: 80,
+    },
+    iconButton: {
+      height: 120,
+      width: 120,
+    },
+    linkedinIcon: {
+      color: '#0077B5',
     },
     toolbarSpacer: theme.mixins.toolbar,
+    twitterIcon: {
+      color: '#00aced',
+    },
   });
 
 interface Props extends WithStyles<typeof styles> {}
+interface ContactItem {
+  component: JSX.Element;
+  to: string;
+}
 
 export default withStyles(styles)(function Contact(props: Props) {
   const { classes } = props;
-  const icons = [
-    <Twitter key="twitter-icon" className={classes.icon} />,
-    <GithubCircle key="github" className={classes.icon} />,
-    <Linkedin key="linkedin" className={classes.icon} />,
+  const contacts: ContactItem[] = [
+    {
+      component: (
+        <Twitter
+          key="twitter-icon"
+          className={classNames(classes.icon, classes.twitterIcon)}
+        />
+      ),
+      to: 'https://twitter.com/sasurau4',
+    },
+    {
+      component: (
+        <GithubCircle
+          key="github"
+          className={classNames(classes.icon, classes.githubIcon)}
+        />
+      ),
+      to: 'https://github.com/sasurau4',
+    },
+    {
+      component: (
+        <Linkedin
+          key="linkedin"
+          className={classNames(classes.icon, classes.linkedinIcon)}
+        />
+      ),
+      to: 'https://www.linkedin.com/in/daiki-ihara-8bb118106',
+    },
   ];
   return (
     <>
@@ -47,22 +83,30 @@ export default withStyles(styles)(function Contact(props: Props) {
         spacing={40}
       >
         <Grid item={true}>
-          <Typography variant="display3" align="center">
+          <Typography variant="display3" align="center" gutterBottom={true}>
             Contact
           </Typography>
         </Grid>
-        <Grid
-          container={true}
-          justify="center"
-          direction="row"
-          alignItems="center"
-          spacing={32}
-        >
-          {icons.map((icon, i) => (
-            <Grid item={true} key={i}>
-              <Avatar className={classes.avatar}>{icon}</Avatar>
-            </Grid>
-          ))}
+        <Grid item={true}>
+          <Grid
+            container={true}
+            justify="center"
+            direction="row"
+            alignItems="center"
+            spacing={32}
+          >
+            {contacts.map((item, i) => (
+              <Grid item={true} key={i}>
+                <IconButton
+                  className={classes.iconButton}
+                  href={item.to}
+                  target="_blank"
+                >
+                  {item.component}
+                </IconButton>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
       </Grid>
     </>
