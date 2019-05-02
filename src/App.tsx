6fd5994aeeb3dee729a865f21e417377a96a3264
Daiki Ihara <sasurau4@gmail.com>
2019-05-02 22:28:20 +0900

    add new items from create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import * as React from 'react';
-import styled from 'styled-components';
-import { Header, Main } from './components';
-import withRoot from './withRoot';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.StatelessComponent<{}> = () => (
-  <RootContainer>
-    <Header />
-    <Main />
-  </RootContainer>
-);
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-export default withRoot(App);
-
-const RootContainer = styled.div`
-  display: flex;
-`;
+export default App;
