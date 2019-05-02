0440aafb99f18e5b3bf07cf426289c59803a9577
Daiki Ihara <sasurau4@gmail.com>
2019-05-02 22:27:55 +0900

    remove old dependencies

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import About from './About';
-import Career from './Career';
-import CareerCard from './CareerCard';
-import Contact from './Contact';
-import Header from './Header';
-import Home from './Home';
-import Main from './Main';
-
-export { About, Home, Header, Main, Contact, Career, CareerCard };
--- src/types/index.tsx
+++
@@ -1,6 +0,0 @@
-export interface CareerItems {
-  headline: string;
-  positions: string[];
-  skills: string[];
-  details: string[];
-}
