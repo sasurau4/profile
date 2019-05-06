e6bfdaf8394d4682125686f9f31ed12e99b4ca08
Daiki Ihara <sasurau4@gmail.com>
2019-05-06 23:07:01 +0900

    Updates

---
+++ precache-manifest.dac2b72c287bc0f75b5b72df58d083c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7464560e7756df0bfdd22d199d0a71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2fdfa913f2eb35c1ebd",
+    "url": "/static/js/2.e7060394.chunk.js"
+  },
+  {
+    "revision": "7e635f205e19fe5e8c0b",
+    "url": "/static/js/main.e60876bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b370f0291a27338d4b21378afdab16a",
+    "url": "/static/media/kirigamine-10_TP_V.0b370f02.jpg"
+  },
+  {
+    "revision": "4991b92e78e1f156fd7ec036c1e1e0da",
+    "url": "/static/media/my-androidify.4991b92e.png"
+  },
+  {
+    "revision": "4a5e9b192b5c06fde7556e6601001dc0",
+    "url": "/static/media/my-profile-picture.4a5e9b19.jpg"
+  }
+]);
