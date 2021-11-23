b87b8e440a8dbf818bbba26dac6b207787d19cc9
Daiki Ihara <sasurau4@gmail.com>
2021-11-23 18:05:50 +0900

    Migrate About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,46 +1,24 @@
-import Avatar from '@mui/material/Avatar';
-import Grid from '@mui/material/Grid';
-import { Theme } from '@mui/material/styles';
-import { WithStyles } from '@mui/styles';
-import createStyles from '@mui/styles/createStyles';
-import withStyles from '@mui/styles/withStyles';
-import Typography from '@mui/material/Typography';
-import * as React from 'react';
-import Androidfy from '../images/my-androidify.png';
-import MyPicture from '../images/my-profile-picture.jpg';
+import Avatar from '@mui/material/Avatar'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+import Androidfy from '../images/my-androidify.png'
+import MyPicture from '../images/my-profile-picture.jpg'
+import Container from '@mui/material/Container'
 
-const styles = (theme: Theme) =>
-  createStyles({
-    avatar: {
-      height: 200,
-      width: 200,
-    },
-    grid: {
-      flexGrow: 1,
-      maxWidth: '100vw',
-      minHeight: '100vh',
-    },
-    // Required Grid component Limitations
-    // see https://material-ui.com/layout/grid/#negative-margin
-    root: {
-      padding: 16,
-    },
-    toolbarSpacer: theme.mixins.toolbar,
-  });
-
-interface Props extends WithStyles<typeof styles> {}
-
-export default withStyles(styles)(function About(props: Props) {
-  const { classes } = props;
+export default function About() {
   return (
-    <div id="about" className={classes.root}>
-      <div className={classes.toolbarSpacer} />
+    <Container id="about">
       <Grid
         container={true}
         justifyContent="center"
         direction="column"
         alignItems="center"
-        className={classes.grid}
+        sx={{
+          flexGrow: 1,
+          maxWidth: '100vw',
+          minHeight: '100vh',
+        }}
         spacing={4}
       >
         <Grid item={true}>
@@ -60,14 +38,20 @@ export default withStyles(styles)(function About(props: Props) {
               <Avatar
                 alt="Daiki Ihara androidfy"
                 src={Androidfy}
-                className={classes.avatar}
+                sx={{
+                  height: 200,
+                  width: 200,
+                }}
               />
             </Grid>
             <Grid item={true}>
               <Avatar
                 alt="Daiki Ihara picture"
                 src={MyPicture}
-                className={classes.avatar}
+                sx={{
+                  height: 200,
+                  width: 200,
+                }}
               />
             </Grid>
           </Grid>
@@ -79,11 +63,11 @@ export default withStyles(styles)(function About(props: Props) {
             color="textSecondary"
             paragraph={true}
           >
-            Daiki Ihara is Front-End Engineer. I love Web, Vim, Javascript,
-            Typescript, React etc. Working in Tokyo, Japan. Like web services.
+            Daiki Ihara is SRE. I love web, programming and working with humans.
+            Working in Tokyo, Japan.
           </Typography>
         </Grid>
       </Grid>
-    </div>
-  );
-});
+    </Container>
+  )
+}
