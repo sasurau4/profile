879eca68014de0d9ac8bb4c0f7235b1bebf13957
Daiki Ihara <sasurau4@gmail.com>
2021-11-23 17:54:53 +0900

    Migrate Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,45 +1,38 @@
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import { Theme } from '@mui/material/styles';
-import { WithStyles } from '@mui/styles';
-import createStyles from '@mui/styles/createStyles';
-import withStyles from '@mui/styles/withStyles';
-import Typography from '@mui/material/Typography';
-import * as React from 'react';
-import Background from '../images/kirigamine-10_TP_V.jpg';
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+import Background from '../images/kirigamine-10_TP_V.jpg'
 
-const styles = (theme: Theme) =>
-  createStyles({
-    backgroundContainer: {
-      backgroundColor: theme.palette.common.black,
+const Home = () => (
+  <Paper
+    id="home"
+    sx={{
+      backgroundColor: 'common.black',
       backgroundImage: `url(${Background})`,
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
-      color: theme.palette.common.white,
+      color: 'common.white',
       height: '100vh',
       justifyContent: 'center',
       textAlign: 'center',
-    },
-    backgroundFilter: {
-      backgroundColor: 'rgba(0,0,0,0.5)',
-      height: '100vh',
-    },
-    grid: {
-      minHeight: '100vh',
-    },
-  });
-
-interface Props extends WithStyles<typeof styles> {}
-
-const Home = withStyles(styles)((props: Props) => (
-  <Paper id="home" className={props.classes.backgroundContainer}>
-    <div className={props.classes.backgroundFilter}>
+    }}
+  >
+    <Box
+      sx={{
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        height: '100vh',
+      }}
+    >
       <Grid
         container={true}
         justifyContent="center"
         direction="column"
         alignItems="center"
-        className={props.classes.grid}
+        sx={{
+          minHeight: '100vh',
+        }}
       >
         <Typography
           variant="h2"
@@ -53,8 +46,8 @@ const Home = withStyles(styles)((props: Props) => (
           Personal biography
         </Typography>
       </Grid>
-    </div>
+    </Box>
   </Paper>
-));
+)
 
-export default Home;
+export default Home
